0b60f53c1bf44b69c3066072d30f9e55a3a5cedf
skaparthi1 <137256345+skaparthi1@users.noreply.github.com>
2024-12-26 18:14:39 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,2 +1,2 @@
-git clone https://github.com/skaparthi1/GoCD.git /home/ubuntu/
-sudo mv /home/ubuntu/HelloWorld.jar /home/ubuntu/ps/
+git clone https://github.com/skaparthi1/GoCD.git
+mv GoCD/HelloWorld.jar /home/ubuntu/ps/
