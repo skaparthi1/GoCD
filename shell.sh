a64a0af08c38db51d9d35811587a267e091109e8
skaparthi1 <137256345+skaparthi1@users.noreply.github.com>
2024-12-27 17:58:26 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -2,16 +2,19 @@
 git clone https://github.com/skaparthi1/GoCD.git
 
 # Define the paths for the JAR files
-V1_JAR_PATH="GoCD/HelloWorld.jar"
-V2_JAR_PATH="/home/ubuntu/prime-square/HelloWorld-V1.jar"  
+V1_JAR_PATH="/home/ubuntu/prime-square/"
+V2_JAR_PATH="GoCD/"  
 
 # Stop Docker service
 echo "Stopping Docker service..."
 sudo systemctl stop docker
 
+# Removing existing JAR
+sudo rm $V1_JAR_PATH/*.jar
+
 # Replace V1 JAR with V2 JAR
-echo "Replacing $V1_JAR_PATH with $V2_JAR_PATH"
-sudo mv $V2_JAR_PATH $V1_JAR_PATH
+echo "Placing $V2_JAR_PATH Jar"
+sudo mv $V2_JAR_PATH/HelloWorld.jar $V1_JAR_PATH
 
 # Start Docker service
 echo "Starting Docker service..."
