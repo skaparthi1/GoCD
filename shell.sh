2999267061e3196440fd245408e44be98b7e1aec
skaparthi1 <137256345+skaparthi1@users.noreply.github.com>
2024-12-27 14:32:53 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/skaparthi1/GoCD.git
-sudo mkdir /home/ubuntu/ps2
-sudo cp GoCD/HelloWorld.jar /home/ubuntu/ps2/
+sudo mkdir /home/ubuntu/ps
+sudo cp GoCD/HelloWorld.jar /home/ubuntu/ps/
